1b6d1e9c83f4659568ecc74d633de21a37be9a72
markm <mark.mcmahon@autodesk.com>
2009-12-26 16:44:16 +0900

    Various cleanups while adding and running the tests Fixed the VerifyFileCount function which was only working for equals based comparisons

--- built_in_commands.py
+++ built_in_commands.py
@@ -17,7 +17,7 @@ RESULT_SUCCESS = 0
 RESULT_FAILURE = 1
 
 
-def VerifyFileCount(file_pattern, count):
+def VerifyFileCount(file_pattern, count = None):
     """Verify that the file count is as specified
 
     filepattern is a glob for the files to count
@@ -32,13 +32,16 @@ def VerifyFileCount(file_pattern, count):
     if not count:
         raise RuntimeError("You must specify a file count.")
 
-    # strip all spaces from the count
-    count = "".join(count)
+    if isinstance(count, list):
+        # strip all spaces from the count
+        count = "".join(str(part) for part in count)
+    else:
+        count = str(count)
 
     num_files = len(glob.glob(file_pattern))
 
     op = operator.eq
-    desc = 'equal'
+    desc = count
 
     if isinstance(count, basestring):
         count = count.strip()
@@ -46,38 +49,35 @@ def VerifyFileCount(file_pattern, count):
         if count.startswith(">="):
             op = operator.ge
             count = count[2:]
-            desc = "greater than or equal"
 
         elif count.startswith("<="):
             op = operator.le
             count = count[2:]
-            desc = "less than or equal"
 
         elif count.startswith(">"):
             op = operator.gt
             count = count[1:]
-            desc = "greater than"
 
         elif count.startswith("<"):
             op = operator.lt
             count = count[1:]
-            desc = "less than"
 
         elif count.startswith("="):
             count = count[1:]
 
         count = int(count)
 
-    if not op(count, num_files):
-        raise RuntimeError("Check Failed - num files %d is NOT %s %d"% (
-            num_files, desc, count))
-    else:
+    if op(num_files, count):
         message = "Check Passed - num files %d is %s %d"
         return RESULT_SUCCESS, message % (num_files, desc, count)
+    else:
+        raise RuntimeError("Check Failed. Counted: %d  expected count: %s"% (
+            num_files, desc))
 
 
-def FileExists(path, dummy):
-    "Check if the file exists"
+def FileExists(path, dummy = None):
+    """Check if the file exists returns 0 if the file exists and raises
+    RuntimeError if the file does not exist"""
 
     if os.path.exists(path):
         return RESULT_SUCCESS, 'SUCCESS: Path exists'
@@ -85,7 +85,7 @@ def FileExists(path, dummy):
         raise RuntimeError("Required file not found: '%s'"% path)
 
 
-def Output(message, dummy):
+def Output(message, dummy = None):
     "Output the message"
 
     LOG.info(message)
@@ -93,11 +93,24 @@ def Output(message, dummy):
     return RESULT_SUCCESS, ''
 
 
-def SystemCommand(command, qualifiers):
-    "Execute a system command, and optionally capture the output"
+def SystemCommand(command, qualifiers = None):
+    """Execute a system command, and optionally capture the output
+    
+    Allowed qualifiers are:
+        - ui      output will not be captured - this allows the user to 
+                  interact with the command (i.e. if it may request the user
+                  to hit a key.
+        - nocheck Do not check the return value. Default is to raise an 
+                  exception if the return value is not 0 (success)
+    """
 
+    # as [] is a dangerous default - replace the default None with []
+    # in the function body
+    if qualifiers is None:
+        qualifiers = []
+
+    # if the 
     new_stdout = sys.stdout
-    print qualifiers
     if 'ui' not in qualifiers:
         new_stdout = tempfile.TemporaryFile()
 
