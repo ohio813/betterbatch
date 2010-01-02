4ba05983423b3bae05cc25ceb999e48826b2e58d
markm <mark.mcmahon@autodesk.com>
2010-01-02 20:41:27 +0900

    change the indentation of the setup.py file

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 VERSION = '1.0'
 
-setup(name='BetterBatch',
-      version=VERSION,
-      requires=['yaml'],
-      download_url=(
+setup(
+    name='BetterBatch',
+    version=VERSION,
+    requires=['yaml'],
+    download_url=(
         'http://www.example.com/pypackage/dist/pkg-%s.tar.gz'% VERSION),
-     )
+    )
