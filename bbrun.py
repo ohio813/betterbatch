d7ba81c677aa36b7ed9c66656c298e026d3fe5e5
markm <mark.mcmahon@autodesk.com>
2010-02-04 23:18:41 +0800

    Update bbrun for betterbatch.parsescript (as the main function does all the error collection work - this script does not have to)

--- bbrun.py
+++ bbrun.py
@@ -1,19 +1,7 @@
-from betterbatch import *
+from betterbatch.parsescript import Main
 
 if __name__ == '__main__':
     #import cProfile
     #import pstats
 
-    try:
-        Main()
-        #cProfile.run('Main()', "profile_stats")
-        #p = pstats.Stats('profile_stats')
-        #p.sort_stats('cumulative').print_stats(10)
-
-    except ErrorCollection, err:
-        err.LogErrors()
-    except RuntimeError, err:
-        LOG.critical(err)
-    except Exception, err:
-        LOG.critical('Unknown Error: %s'% err)
-        LOG.exception(err)
+    Main()
