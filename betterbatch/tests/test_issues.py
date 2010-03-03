f618cf3a042a3fad4363f6aee9aca134712498a0
markm <markm@localhost>
2010-03-03 15:36:52 +0800

    Add a test (which does not test anything for the issue raised on the discussion list - that an error is raised when the tool folder does not exist)

--- betterbatch/tests/test_issues.py
+++ betterbatch/tests/test_issues.py
@@ -18,14 +18,26 @@ TEST_FILES_PATH = os.path.join(TEST_PATH, "test_files")
 class Issue_tests(unittest.TestCase):
 
     def test_issue1(self):
-        """"""
+        """Test that a variable can be updated in a for loop"""
         full_path = os.path.join(TEST_FILES_PATH, 'issue1.bb')
-        steps = ExecuteScriptFile(full_path, {})
+        steps, vars = ExecuteScriptFile(full_path, {})
         
         self.assertEquals(
             len(steps[-1].output.split("+")) > 5,
             True)
             
+    def test_distribution_list_Yuhui(self):
+        """Test that an missing tools directory does not raise a problem"""
+        
+        # this is not so easy to test, I don't particulary want to rename
+        # the folder on my machine - and would like that "add_tools_dir" still
+        # raises an error when the folder doesn't exist.
+        # so for now this test doesn't do anything :(
+        #
+        # The issue was fixed by testing that the tools folder exists before
+        # calling PopulateFromToolsFolder
+        pass
+
 
 
 if __name__ == "__main__":
