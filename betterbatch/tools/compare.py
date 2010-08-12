9a3d22a9bb2f42ae894f915ba585f3c8fa4d5697
markm <markm@localhost>
2010-08-12 17:04:46 +0800

    Remove trailing whitespace

--- betterbatch/tools/compare.py
+++ betterbatch/tools/compare.py
@@ -25,44 +25,44 @@ def ParseComparisonOperator(op_text):
 
     elif op_text.startswith("!="):
         op = operator.ne
-    
+
     elif op_text.lower() == "startswith":
         op = unicode.startswith
-        
+
     elif  op_text.lower() == "endsswith":
         op = unicode.endswith
-    
+
     elif  op_text.lower() == "contains":
         op = unicode.__contains__
-    
+
     elif op_text.lower() == "matches_regex":
         op = RegularExpressionTest
-    
+
     else:
         raise RuntimeError("Unknown comparison type '%s'"% op_text)
-    
+
     return op
 
 def RunComparison(str1, operator, str2):
     return operator(str1, str2)
-    
+
 if __name__ == "__main__":
     str1 = unicode(sys.argv[1])
     op_text = sys.argv[2]
     str2 = unicode(sys.argv[3])
-        
+
     qualifiers = [q.lower() for q in sys.argv[4:]]
 
     #print "---------------", (str1, op_text, str2, qualifiers)
-    
+
     if "nocase" in qualifiers:
         str1 = str1.lower()
         str2 = str2.lower()
-    
+
     if "asint" in qualifiers:
         str1 = int(str1)
         str2 = int(str2)
-    
+
     op = ParseComparisonOperator(op_text)
     
     ret = RunComparison(str1, op, str2)
@@ -73,5 +73,3 @@ if __name__ == "__main__":
     else:
         ret = 1
     sys.exit(ret)
-    
-    
