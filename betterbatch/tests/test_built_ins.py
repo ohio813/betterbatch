96b350ce6ddeaead39f9fb91753579aad1bb1026
markm <markm@localhost>
2011-03-13 20:49:43 +0800

    Remove trailing spaces

--- betterbatch/tests/test_built_ins.py
+++ betterbatch/tests/test_built_ins.py
@@ -55,10 +55,10 @@ class BuiltInCommandsTests(unittest.TestCase):
 
     def test_count_fail(self):
         """"""
-        
+
         ret, out = VerifyFileCount("cmd_line.py", 12)
         self.assertNotEquals(ret, 0)
-        
+
 
     def test_missing_count(self):
         self.assertRaises(
@@ -73,13 +73,13 @@ class BuiltInCommandsTests(unittest.TestCase):
         SystemCommand("echo here", [])
 
     def test_SystemCommand_nocapture(self):
-        SystemCommand("echo here",  ['nocapture'])    
-    
+        SystemCommand("echo here",  ['nocapture'])
+
     def test_SystemCommand_echo(self):
-        SystemCommand("echo here",  ['echo'])    
+        SystemCommand("echo here",  ['echo'])
 
     def test_SystemCommand_echo_nocapture(self):
-        SystemCommand("echo here",  ['echo', 'nocapture'])    
+        SystemCommand("echo here",  ['echo', 'nocapture'])
 
     def test_SystemCommand_no_too_long(self):
         self.assertRaises(
@@ -96,12 +96,12 @@ class BuiltInCommandsTests(unittest.TestCase):
     def test_ChangeCurrentDirectory_pass(self):
         """"""
         cur_dir = os.path.abspath(os.getcwd())
-        
+
         ret, out = ChangeCurrentDirectory("\\")
-        
+
         self.assertEquals((ret, out), (0, ""))
         self.assertEquals(os.getcwd(), os.path.abspath("\\"))
-        
+
         # change it back
         ChangeCurrentDirectory(cur_dir)
 
@@ -119,36 +119,36 @@ class BuiltInCommandsTests(unittest.TestCase):
     def test_ChangeCurrentDirectory_fail(self):
         """Test the "cd" command that will fail"""
         cur_dir = os.path.abspath(os.getcwd())
-        
+
         ret, out = ChangeCurrentDirectory("\\non_existing_directory_somewhere")
-        
+
         self.assertNotEquals(ret, 0)
         self.assertEquals(os.getcwd(), cur_dir)
 
     def test_ChangeCurrentDirectory_list(self):
         """Test the "cd" command that will fail"""
         cur_dir = os.path.abspath(os.getcwd())
-        
+
         ret, out = ChangeCurrentDirectory(["\\"])
-        
+
         self.assertEquals(ret, 0)
         self.assertEquals(os.path.splitdrive(os.getcwd())[1], "\\" )
 
     def test_PushDirectory_pass(self):
         cur_dir = os.path.abspath(os.getcwd())
-        
+
         ret, out = PushDirectory("\\")
-        
+
         self.assertEquals((ret, out), (0, ""))
         self.assertEquals(os.getcwd(), os.path.abspath("\\"))
-        
+
         # change it back
         PopDirectory()
-        
+
     def test_PushDirectory_fail(self):
         """Try to push a directory that doesn't exist"""
         cur_dir = os.path.abspath(os.getcwd())
-        
+
         ret, out = PushDirectory("\\non_existing_directory_somewhere")
 
         self.assertNotEquals((ret, out), (0, ""))
@@ -177,7 +177,7 @@ class BuiltInCommandsTests(unittest.TestCase):
         self.assertRaises(
             RuntimeError,
             PopDirectory)
-        
+
         PushDirectory("\\")
         PushDirectory("\\")
         PopDirectory()
@@ -186,24 +186,24 @@ class BuiltInCommandsTests(unittest.TestCase):
         self.assertRaises(
             RuntimeError,
             PopDirectory)
-        
+
     def test_PopDirectory_fail2(self):
         # simulate that a directory stored (in a PushDirectory call) has
         # been removed since
-        
+
         built_in_commands.PUSH_DIRECTORY_LIST.append("not_here_at_all")
         
         ret, out = PopDirectory()
-        
+
         self.assertNotEquals(ret, 0)
         
     def test_ExternalCommand_pass(self):
         tool_path = os.path.join(package_root, "tools", "GetLanguage.py")
         ec = ExternalCommand(tool_path)
-        
+
         #print tool_path
         #print ec(["a", "=", "A", "nocase"])
-        
+
         expected_ret = (0, 'de-DE\r\n')
         self.assertEquals(ec("deu  dotnet"), expected_ret)
         self.assertEquals(ec("deu dotnet", ["nocase"]), expected_ret)
@@ -213,9 +213,9 @@ class BuiltInCommandsTests(unittest.TestCase):
                 ("a", "=", "A", "nocase"))
         self.assertRaises(
             RuntimeError,
-            ec, 
+            ec,
                 ["a", "=", "A"], ["nocase"])
-            
+
     def test_ExternalCommand_missing_cmd(self):
         """Test external path with tool that doesn't exitst"""
         
@@ -229,7 +229,7 @@ class BuiltInCommandsTests(unittest.TestCase):
         "   "
         tool_path = os.path.join(package_root, "tools", "getlanguage.py")
         ec = ExternalCommand(tool_path)
-        
+
         self.assertRaises(
             RuntimeError,
             ec,
@@ -247,7 +247,7 @@ class BuiltInCommandsTests(unittest.TestCase):
             RuntimeError,
             PopulateFromToolsFolder,
                 tools_dir)
-        
+
         del(built_in_commands.NAME_ACTION_MAPPING['compare'])
 
     def test_Split_no_split_text(self):
