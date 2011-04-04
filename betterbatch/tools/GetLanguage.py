8136522b8e20c0d7f5852487b505fa95659f28d3
markm <markm@localhost>
2011-04-04 15:33:45 +0800

    Add more languages, support leading comments and blank lines. Strip trailing spaces from the code Return an error code if there is no value for a particular language/format pair

--- betterbatch/tools/GetLanguage.py
+++ betterbatch/tools/GetLanguage.py
@@ -14,50 +14,57 @@ import csv
 
 def ReadLangInfoDB(lang_info_filepath = None):
     "Load the CSV file with the language information"
-    
-    # if no file specified then default to langinfo.csv in the same 
+
+    # if no file specified then default to langinfo.csv in the same
     # folder as the script
     if lang_info_filepath is None:
         lang_info_filepath = os.path.join(
-            os.path.dirname(__file__), 'langinfo.csv')        
-    
-    # open the file and initialize the CSV DictReader 
+            os.path.dirname(__file__), 'langinfo.csv')
+
+    # open the file and initialize the CSV DictReader
     lang_info_file = open(lang_info_filepath, "rb")
+    # skip leading comments and blank lines
+    while True:
+        line_start = lang_info_file.tell()
+        line = lang_info_file.readline()
+        if line.strip() and not line.strip().startswith(';'):
+            lang_info_file.seek(line_start)
+            break
+
     csv_reader = csv.DictReader(lang_info_file)
-    
+
     # each entry from DictReader will be a dictionary
-    # 
+    #
     lang_info = {}
     for line in csv_reader:
         line['culture'] = line['dotnet'][:2]
         lang_info[line['lang'].lower()] = line
-        
 
     lang_info_file.close()
 
-    return lang_info   
+    return lang_info
 
 
 def GetLangInfo(lang, requested_format):
     "Return  the information "
 
     languages_db = ReadLangInfoDB()
-    
+
     if lang not in languages_db:
-        raise RuntimeError ("Unknown language: '%s' Known languages: %s"% 
+        raise RuntimeError ("Unknown language: '%s' Known languages: %s"%
             (lang, ", ".join(languages_db.keys()) ) )
 
     language_data = languages_db[lang]
-    
+
     if requested_format not in language_data:
-        raise RuntimeError ("Unknown format: '%s' Known formats: %s"% 
+        raise RuntimeError ("Unknown format: '%s' Known formats: %s"%
             (requested_format, ", ".join(language_data.keys()) ) )
-    
+
     return language_data[requested_format]
 
 if __name__ == "__main__":
     import sys
-    
+
     try:
         req_lang = sys.argv[1].lower()
         req_format = sys.argv[2].lower()
@@ -66,11 +73,16 @@ if __name__ == "__main__":
         sys.exit(1)
 
     try:
-        print GetLangInfo(req_lang, req_format)
+        lang_info = GetLangInfo(req_lang, req_format)
+        if not lang_info.strip():
+            print "Language '%s' has no setting for '%s'" % (
+                req_lang, req_format)
+            sys.exit(2)
+        print lang_info
     except RuntimeError, e:
         print e
         sys.exit(1)
-    
+
     # successfully done
     sys.exit(0)
- 
+
