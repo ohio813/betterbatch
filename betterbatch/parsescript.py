95b4cd08738436e9bd4429889efea6381a25ecb2
markm <markm@localhost>
2011-04-06 11:38:58 +0800

    Add END and ECHO statements to those that are set to pre-formatted by default

--- betterbatch/parsescript.py
+++ betterbatch/parsescript.py
@@ -167,9 +167,11 @@ def ParseYAMLFile(yaml_file):
         # avoid the 'double quote parsing of YAML'
         yaml_data = yaml_data.replace('"', '+++++dblquote+++++')
 
-        #ensure that USAGE blocks are treated as pre-formatted strings
-        usage_block = re.compile("^(\s*)-(\s+set\s+usage.*$)", re.I | re.M)
-        yaml_data = usage_block.sub(r"\1- |\n\1 \2", yaml_data)
+        # ensure that USAGE blocks, echo or end statemetns are treated
+        # as pre-formatted strings
+        usage_block = re.compile(
+            "^(\s*)-\s+((set\s+usage|echo|end)\s+.*$)", re.I | re.M)
+        yaml_data = usage_block.sub(r"\1- |\n\1    \2", yaml_data)
 
         # allow new-lines in {{{ }}} quoted strings
         brace_quoted = re.compile("\{\{\{.*?\}\}\}", re.DOTALL)
